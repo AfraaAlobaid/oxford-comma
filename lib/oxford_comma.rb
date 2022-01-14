aa31fda1699ca49eee6695c4bd334371f649ca4c
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-14 15:59:05 +1030

    Performed string manipulations

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+    string = ""
 
+    if array.size == 1
+        return array[0]
+    elsif array.size == 2
+        return "#{array[0]} and #{array[1]}"
+    else 
+        array.each_with_index do |element, index|
+            if index > 0 && index < array.size-1 
+                string << ", "
+            elsif index == array.size-1
+                string << ", and "
+            end
+            string << element
+        end 
+    end
+    return string
 end
